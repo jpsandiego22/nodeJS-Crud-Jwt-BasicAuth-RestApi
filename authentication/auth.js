2f145275a5cece6fb586e894659bf6ddee2725ee
jpsandiego22 <jomersan0619@gmail.com>
2024-03-13 20:46:50 +0800

    Repositioning

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,6 +1,10 @@
 const uuid = require('uuid');
 const jwt = require('jsonwebtoken');
 const usedTokens = new Set();
+const bAuthUsername = process.env.BASIC_AUTH_USERNAME;
+const bAuthPassword = process.env.BASIC_AUTH_PASSWORD;
+const secretKey = process.env.SECRET_KEY;
+
 const bearerTokenRequest = (req,res) =>{
     const authHeader = req.headers['authorization'];
 
@@ -17,42 +21,31 @@ const bearerTokenRequest = (req,res) =>{
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [username, password] = credentials.split(':');
     
-    const bAuthUsername = process.env.BASIC_AUTH_USERNAME;
-    const bAuthPassword = process.env.BASIC_AUTH_PASSWORD;
+    
  
     if (bAuthUsername !== username && bAuthPassword !== password) {
         return res.status(401).json({ message: 'Invalid credentials' });
     }
-
-    const secretKey = process.env.SECRET_KEY;
+    
     const tokenIdentifier = uuid.v4();
     const token = jwt.sign({ username: bAuthUsername, tokenIdentifier }, secretKey, { expiresIn: '1h' });
     res.json({ message:"success",token });
-
 }
 
 const validateToken = (req,res,next)=>{
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    const secretKey = process.env.SECRET_KEY;
-
     if (!token) {
         return res.sendStatus(401);
     }
-
     jwt.verify(token, secretKey, (err, decoded) => {
         if (err) {
             return res.sendStatus(403);
         }
-
         if (usedTokens.has(decoded.tokenIdentifier)) {
-            // Token has been used before
-            return res.status(401).json({ message: 'Token has already been used' });
+            return res.status(401).json({ message: 'Unauthorized Access.' });
         }
-
-        // Mark token as used
         usedTokens.add(decoded.tokenIdentifier);
-        
         next();
     });
 };
